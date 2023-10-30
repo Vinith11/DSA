7fbb01ba661531ba82b3ecb7f04485359bedce3f
Vinith <vinith2003@gmail.com>
2023-10-30 19:12:17 +0530

    push

---
+++ Patterns/p2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i < 6; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "*";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
