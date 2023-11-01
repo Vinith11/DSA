d897cff39e4498176d00d4a937a5a347a5fee38e
Vinith <vinith2003@gmail.com>
2023-11-01 13:25:20 +0530

    push

--- _283_move_zero.py
+++ _283_move_zero.py
@@ -6,7 +6,21 @@ class Solution(object):
                 
                 nums.remove(0)
                 nums.append(0)
-            
+
+'''
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int h=0;
+        for(int i=0; i<nums.size(); i++){
+            if (nums[i] != 0) {
+                swap(nums[i], nums[h]);
+                h++;
+            }
+        }
+    }
+};
+'''
         
       
             
