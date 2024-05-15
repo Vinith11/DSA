d0a4282ab969fa8841a6779da53992b8d71d3e14
Vinith11 <vinith2003@gmail.com>
2024-05-15 13:55:05 +0530

    feat: Refactor _78_Subsets to use backtracking algorithm

--- LeetCode150/_78_Subsets.java
+++ LeetCode150/_78_Subsets.java
@@ -4,36 +4,31 @@ import java.util.List;
 
 public class _78_Subsets {
 
-    public static void solve(int index, int[] nums, List<List<Integer>> result,List<Integer> output) {
-        //base case
-        if (index >= nums.length) {
-            result.add(new ArrayList<>(output));
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
+        backtrack(nums, output, ans, 0);
+        return ans;
+    }
+
+    private static void backtrack(int[] nums, List<Integer> output, List<List<Integer>> ans, int index) {
+        if (index == nums.length) {
+            ans.add(new ArrayList<>(output));
             return;
         }
 
+        // exclude
+        backtrack(nums, output, ans, index + 1);
+
+        // include
         output.add(nums[index]);
-        //exclude
-        solve(index+1, nums, result, output);
-
-        //include
-        output.remove(output.size()-1);
-
-        solve(index+1, nums, result, output);
-    }
-
-    public static List<List<Integer>> method1(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> output = new ArrayList<>();
-
-        solve(0, nums, result, output);
-
-        return result;
-
+        backtrack(nums, output, ans, index + 1);
+        output.remove(output.size() - 1);
     }
 
     public static void main(String[] args) {
         int[] nums = {1, 2, 3};
-        List<List<Integer>> result = method1(nums);
+        List<List<Integer>> result = subsets(nums);
         System.out.println(result);
     }
 }
